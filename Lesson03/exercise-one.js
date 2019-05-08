231dc546af9b06b51736cf445669095b6c008952
ymmy1 <chem-pion@mail.ru>
2019-05-08 13:47:02 -0700

    added console.log for each item[bug fix]

--- Lesson03/exercise-one.js
+++ Lesson03/exercise-one.js
@@ -1,6 +1,7 @@
 const workingWeek = ['Monday', 'Tuesday', 'Saturday', 'Wednesday', 'Thursday'];
 
 workingWeek.push('Friday.');
+console.log(workingWeek);
 workingWeek.splice(2,1);
 
 const workingWeekList = workingWeek.join(', ');
